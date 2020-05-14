dc856e3986473eefa491629bb7e019b6d62a6806
Christopher Chewa <chrischewa@gmail.com>
2020-05-14 21:57:11 +0300

    14thMay2200hrs

--- mainapp/filters.py
+++
@@ -1,8 +0,0 @@
-from .models import PropertyEntry
-import django_filters
-
-class UserFilter(django_filters.FilterSet):
-    class Meta:
-        model = PropertyEntry
-        fields = ['property_type', 'price']
-
